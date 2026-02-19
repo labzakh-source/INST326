30abe2319e7a4bf85520338be6a9d5d0dec2cc41
labzakh-source <labzakh@terpmail.umd.edu>
2026-02-18 23:29:17 -0500

    Rah Rah she's done

--- finder.py
+++ finder.py
@@ -2,24 +2,29 @@ import sys
 import argparse
 """
 Driver: Lina Abzakh
-Navigator:Jayant Ranabhat
+Navigator: Jayant Ranabhat
 Assignment """
 
-HOME_DATABASE = {
-"name":["shoes", "papers", "picture frames", "tshirts", "soccer balls", "kitchen tables", "cabinets"],
-"location":["floor", "cubbard", "wall", "cubbard", "basket", "floor", "corner"],
-"date last moved": ["8/23/23", "7/23/23", "6/23/23", "1/13/23", "2/9/22", "3/23/23", "5/30/23"],
-"time last moved": ["10:00am", "1:00am", "10:00pm", "9:00am", "12:00pm", "4:00pm", "10:30pm"]
-}
+HOME_DATABASE = """shoes,floor,8/23/23,10:00am
+papers,cubbard,7/23/23,1:00am
+picture frames,wall,6/23/23,10:00pm
+tshirts,cubbard,1/13/23,9:00am
+soccer balls,basket,2/9/22,12:00pm
+kitchen tables,floor,3/23/23,4:00pm
+cabinets,corner,5/30/23,10:30pm"""
 
-def main(NEW_DATABASE)
-    name = input("What item are you looking for?: ")
-    NEW_DATABASE = {}
-    output = f"The {name} were found in the {location} and were placed there on {date last moved} at {time
-    last moved}"
-    return output
+def main(name):
+    #separates every new line
+    database_lines = HOME_DATABASE.split("\n")
+    #iterates through newly separated database and assigns values to each word with the "," separating indicating a new value has started
+    for line in database_lines:
+        item_name, location, date_last_moved, time_last_moved = line.split(",")
+    #if value matches that of the users input, a message is returned, and if it isn't it raises a ValueError
+        if item_name == name:
+            return f"The {name} were found in the {location} and were placed there on {date_last_moved} at {time_last_moved}"
+    raise ValueError(f"Sorry, could not find your item named {name} within the database")
+ 
     
-
 
     
 
